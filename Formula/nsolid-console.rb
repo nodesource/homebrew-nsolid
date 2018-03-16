6adadddf4a0368a900bb3da5e3b2b97645f9d794
Graham Krizek <gkrizek@nodesource.com>
2018-03-16 09:31:21 -0600

    Add plist file for brew services

--- Formula/nsolid-console.rb
+++ Formula/nsolid-console.rb
@@ -15,12 +15,41 @@ class NsolidConsole < Formula
     prefix.install Dir["*"]
   end
 
-  def post_install
-    ohai "N|Solid Console installed successfully."
-    ohai ""
-    ohai "To start the console run the command 'nsolid-console'"
-    ohai ""
-    ohai "After the console is running, you can view it at 'http://localhost:6753'"
-    ohai ""
+  def caveats
+    s = <<~EOS
+      N|Solid Console installed successfully.
+
+      To start the console run the command 'nsolid-console'
+
+      After the console is running, you can view it at 'http://localhost:6753'
+    EOS
+    s
   end
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/nsolid-console</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardOutPath</key>
+      <string>/tmp/nsolid-console.log</string>
+      <key>StandardErrorPath</key>
+      <string>/tmp/nsolid-console.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
 end
