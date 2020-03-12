d201f969fc31dd4ba1f5c835f1193cc998cc8757
igsu <ivigsu@gmail.com>
2020-03-12 13:31:57 -0500

    3.10.0

--- Formula/nsolid-console.rb
+++ Formula/nsolid-console.rb
@@ -1,9 +1,9 @@
 class NsolidConsole < Formula
   desc "N|Solid Console"
   homepage "https://nodesource.com/products/nsolid"
-  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/3.9.0/artifacts/bundles/nsolid-bundle-v3.9.0-darwin-x64/nsolid-console-v3.9.0-darwin-x64.tar.gz"
-  sha256 "3cc7020d0a0a2ab0d72c57c04357cd6d7abc0674f677931bbcdc74f36864c90f"
-  version "3.9.0"
+  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/3.10.0/artifacts/bundles/nsolid-bundle-v3.10.0-darwin-x64/nsolid-console-v3.10.0-darwin-x64.tar.gz"
+  sha256 "2a5b2d74d1a9bee871f031b902b061e4b481e8b96543c2ebae50ee4e547e57b0"
+  version "3.10.0"
 
   depends_on :macos => :yosemite
   depends_on "nsolid"
--- Formula/nsolid.rb
+++ Formula/nsolid.rb
@@ -1,9 +1,9 @@
 class Nsolid < Formula
   desc "N|Solid Runtime"
   homepage "https://nodesource.com/products/nsolid"
-  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/3.9.0/artifacts/bundles/nsolid-bundle-v3.9.0-darwin-x64/nsolid-v3.9.0-dubnium-darwin-x64.tar.gz"
-  sha256 "730ccc68df4e90780c930acfbff19e8e49f428ee87c2ae439cf18ec5d64ea796"
-  version "3.9.0"
+  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/3.10.0/artifacts/bundles/nsolid-bundle-v3.10.0-darwin-x64/nsolid-v3.10.0-dubnium-darwin-x64.tar.gz"
+  sha256 "7c6e87d9d63fb52dcb36ed3e0ff681c4dd9422fbbe9c6984f2c57c60aed201d7"
+  version "3.10.0"
 
   conflicts_with "node", :because => "N|Solid is a replacement for NodeJS"
   depends_on :macos => :yosemite
