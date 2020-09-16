ae041e4a0ae3f2ed68918d11b7f649f9a058bced
Ivan Iguaran <ivigsu@gmail.com>
2020-09-16 13:51:36 -0500

    4.0.1

--- Formula/nsolid.rb
+++ Formula/nsolid.rb
@@ -1,9 +1,9 @@
 class Nsolid < Formula
   desc "N|Solid Runtime Erbium"
   homepage "https://nodesource.com/products/nsolid"
-  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.0.0/artifacts/bundles/nsolid-bundle-v4.0.0-darwin-x64/nsolid-v4.0.0-erbium-darwin-x64.tar.gz"
-  sha256 "2d9f336061c4c9a329c22224c1106c8e37761d239e6c7b0421fbbf38d9b56723"
-  version "4.0.0"
+  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.0.1/artifacts/bundles/nsolid-bundle-v4.0.1-darwin-x64/nsolid-v4.0.1-erbium-darwin-x64.tar.gz"
+  sha256 "446f3f63b684c2ed893d868e0ed2fc2050ae6bcde79741333b400d7c21cf63e9"
+  version "4.0.1"
 
   conflicts_with "node", :because => "N|Solid is a replacement for NodeJS"
   conflicts_with "nsolid-dubnium", :because => "N|Solid Erbium is a replacement for N|Solid Dubnium"
