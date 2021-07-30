aafd992a4422d49b0c73b618b1fdc7a9a5322418
Ivan Iguaran <ivigsu@gmail.com>
2021-07-30 17:47:45 -0400

    4.5.7

--- Formula/nsolid-console.rb
+++ Formula/nsolid-console.rb
@@ -1,9 +1,9 @@
 class NsolidConsole < Formula
   desc "N|Solid Console"
   homepage "https://nodesource.com/products/nsolid"
-  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.5.6/artifacts/bundles/nsolid-bundle-v4.5.6-darwin-x64/nsolid-console-v4.5.6-darwin-x64.tar.gz"
-  sha256 "454f0a7d0e1208afda18d510798809684eee06252176ab0a5836ab926787f7e0"
-  version "4.5.6"
+  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.5.7/artifacts/bundles/nsolid-bundle-v4.5.7-darwin-x64/nsolid-console-v4.5.7-darwin-x64.tar.gz"
+  sha256 "d4573e13fe9002234380f8f7ecff0db56bc508e864ff6b6d1eebbdc21df681f1"
+  version "4.5.7"
 
   depends_on :macos => :yosemite
   depends_on "nsolid"
--- Formula/nsolid-erbium.rb
+++ Formula/nsolid-erbium.rb
@@ -1,9 +1,9 @@
 class NsolidErbium < Formula
   desc "N|Solid Runtime Erbium"
   homepage "https://nodesource.com/products/nsolid"
-  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.5.6/artifacts/bundles/nsolid-bundle-v4.5.6-darwin-x64/nsolid-v4.5.6-erbium-darwin-x64.tar.gz"
-  sha256 "970b910784a6b0a987d23ccbaac317833fa79553bd13d639faf4cbf4d842506d"
-  version "4.5.6"
+  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.5.7/artifacts/bundles/nsolid-bundle-v4.5.7-darwin-x64/nsolid-v4.5.7-erbium-darwin-x64.tar.gz"
+  sha256 "1bcf8994444eba6678505d8e074127a731cfcf98e553f0f6bb9cbb46bdc1612b"
+  version "4.5.7"
 
   conflicts_with "node", :because => "N|Solid is a replacement for NodeJS"
   conflicts_with "nsolid", :because => "N|Solid Erbium is a replacement for N|Solid Fermium"
