bab93792419e203215f117a154e1877a7761f8eb
Jefferson <jefferson@nodesource.com>
2022-10-11 15:58:49 -0400

    chore: support for hydrogen

--- Formula/nsolid-gallium.rb
+++ Formula/nsolid-gallium.rb
@@ -1,12 +1,12 @@
 class Nsolid < Formula
-  desc "N|Solid Runtime Hydrogen"
+  desc "N|Solid Runtime Gallium"
   homepage "https://nodesource.com/products/nsolid"
-  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.8.2/artifacts/bundles/nsolid-bundle-v4.8.2-darwin-x64/nsolid-v4.8.2-hydrogen-darwin-x64.tar.gz"
+  url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/4.8.2/artifacts/bundles/nsolid-bundle-v4.8.2-darwin-x64/nsolid-v4.8.2-gallium-darwin-x64.tar.gz"
   sha256 "d013afe3e47f5580384f0b8f6a4e6fc2e4b4557d7c1dcf4973c23b0016a408f1"
   version "4.8.2"
 
   conflicts_with "node", :because => "N|Solid is a replacement for NodeJS"
-  conflicts_with "nsolid-gallium", :because => "N|Solid Hydrogen is a replacement for N|Solid Gallium"
+  conflicts_with "nsolid-fermium", :because => "N|Solid Gallium is a replacement for N|Solid Fermium"
   depends_on :macos => :yosemite
 
   option "without-node", "Won't symlink node, npm, and npx in /usr/local/bin/ to the N|Solid versions"
