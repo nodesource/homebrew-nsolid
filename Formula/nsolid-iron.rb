7533a3267875f878978353713183b99f64e6d9c1
JesusPaz <JesusPaz@users.noreply.github.com>
2024-06-26 17:16:23 +0000

    Automated Release - v20.15.0-ns5.3.0

--- Formula/nsolid-iron.rb
+++ Formula/nsolid-iron.rb
@@ -3,11 +3,11 @@ class NsolidIron < Formula
   homepage "https://nodesource.com/products/nsolid"
 
   if Hardware::CPU.intel?
-    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/20.14.0-ns5.2.3/artifacts/binaries/nsolid-v5.2.3-iron-darwin-x64.tar.gz"
-    sha256 "14660430045c3566bcf3555f82b58ac41ec4ae42706c9dad33078e79fa3d9c32"
+    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/20.15.0-ns5.3.0/artifacts/binaries/nsolid-v5.3.0-iron-darwin-x64.tar.gz"
+    sha256 "c79abc01f2213b6ac1fb8bbb986275921dfa7b31bce9ebc92ff077177862de4d"
   elsif Hardware::CPU.arm?
-    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/20.14.0-ns5.2.3/artifacts/binaries/nsolid-v5.2.3-iron-darwin-arm64.tar.gz"
-    sha256 "109b3e5b8d97df31fd91e35fcef2e4c962802a6c6bd2ce4d700c5a2be4a2a246"
+    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/20.15.0-ns5.3.0/artifacts/binaries/nsolid-v5.3.0-iron-darwin-arm64.tar.gz"
+    sha256 "3658fefc8b11a5d126338a96e00c0d0fc3752ea343ea58afe1d72445cb870ab5"
   end
 
   conflicts_with "node", because: "N|Solid replaces NodeJS"
