c181e7af0d41f81d8edca30903d6145abae1c022
ns-control-tower <27798015+ns-control-tower@users.noreply.github.com>
2026-01-15 23:59:56 +0000

    Automated Release - v24.13.0-ns6.2.0

--- Formula/nsolid-krypton.rb
+++ Formula/nsolid-krypton.rb
@@ -3,11 +3,11 @@ class NsolidKrypton < Formula
   homepage "https://nodesource.com/products/nsolid"
 
   if Hardware::CPU.intel?
-    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/24.11.1-ns6.1.1/artifacts/binaries/nsolid-v6.1.1-krypton-darwin-x64.tar.gz"
-    sha256 "009849364aac9d4bad286d108995e7fae1d29e0067323dc3a7c42435118cb603"
+    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/24.13.0-ns6.2.0/artifacts/binaries/nsolid-v6.2.0-krypton-darwin-x64.tar.gz"
+    sha256 "ee2acd44401beb20a1b5b5caa1865e9c5b096dfd7a00bf2392a75169942c74ba"
   elsif Hardware::CPU.arm?
-    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/24.11.1-ns6.1.1/artifacts/binaries/nsolid-v6.1.1-krypton-darwin-arm64.tar.gz"
-    sha256 "8471c5b94e1ae150e5d3ebf5eaf356e456f2306769627b112e9d18dc9c743630"
+    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/24.13.0-ns6.2.0/artifacts/binaries/nsolid-v6.2.0-krypton-darwin-arm64.tar.gz"
+    sha256 "e5633789ae4930da978cf941cd98bef19a1293eb4c666c467cac02740e7230ab"
   end
 
   conflicts_with "node", because: "N|Solid replaces NodeJS"
