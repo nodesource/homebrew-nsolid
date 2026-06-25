f154e6ab89895b66caf19c9a889e349883a9d3cb
riosje <55195249+riosje@users.noreply.github.com>
2026-06-25 23:41:22 +0000

    Automated Release - v22.23.1-ns6.3.2

--- Formula/nsolid-jod.rb
+++ Formula/nsolid-jod.rb
@@ -3,11 +3,11 @@ class NsolidJod < Formula
   homepage "https://nodesource.com/products/nsolid"
 
   if Hardware::CPU.intel?
-    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/22.23.0-ns6.3.1/artifacts/binaries/nsolid-v6.3.1-jod-darwin-x64.tar.gz"
-    sha256 "24ae56f876a4aa1937ae33823054855af55e701e1db83d230c62b435ef9ec0d7"
+    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/22.23.1-ns6.3.2/artifacts/binaries/nsolid-v6.3.2-jod-darwin-x64.tar.gz"
+    sha256 "5d4dce82b41618198f4447f3d52cc23db81a183eaafe7b8f493dca99faf6bd61"
   elsif Hardware::CPU.arm?
-    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/22.23.0-ns6.3.1/artifacts/binaries/nsolid-v6.3.1-jod-darwin-arm64.tar.gz"
-    sha256 "a837c0984d86c6c1e3e9b401d38fd32b9615354b6c94a86f16a524c0eef2db2c"
+    url "https://s3-us-west-2.amazonaws.com/nodesource-public-downloads/22.23.1-ns6.3.2/artifacts/binaries/nsolid-v6.3.2-jod-darwin-arm64.tar.gz"
+    sha256 "f922c2d42f406d43571adbb6e2ab54ac33af971bd63a0d918f7f7a2e714234ff"
   end
 
   conflicts_with "node", because: "N|Solid replaces NodeJS"
